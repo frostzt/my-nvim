b0c58d9cf858840e3e19c62b216cca7f1f0b1cb6
Sourav Rawat <sourav@cloudflare.com>
2025-12-03 14:37:13 +0530

    chore: updates

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   lazy = false,
@@ -15,6 +15,9 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
