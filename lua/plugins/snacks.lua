35e055ef63b28b2b2771bd54034b5e8b07d43f12
frostzt <aidenfrostbite@gmail.com>
2025-12-05 19:28:23 +0530

    feat(snacks-git): added shortcuts for snacks git

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,7 +2,12 @@ return {
   'folke/snacks.nvim',
   lazy = false,
   priority = 1000,
-  opts = { input = {}, picker = {}, terminal = {}, lazygit = { enabled = true } },
+  opts = {
+    input = {},
+    picker = { enabled = true },
+    terminal = {},
+    lazygit = { enabled = true },
+  },
   keys = {
     {
       '<leader>gg',
@@ -10,6 +15,57 @@ return {
         Snacks.lazygit()
       end,
       desc = 'Lazygit',
+    },
+
+    -- Git
+    {
+      '<leader>gb',
+      function()
+        Snacks.picker.git_branches()
+      end,
+      desc = 'Git Branches',
+    },
+    {
+      '<leader>gl',
+      function()
+        Snacks.picker.git_log()
+      end,
+      desc = 'Git Log',
+    },
+    {
+      '<leader>gL',
+      function()
+        Snacks.picker.git_log_line()
+      end,
+      desc = 'Git Log Line',
+    },
+    {
+      '<leader>gs',
+      function()
+        Snacks.picker.git_status()
+      end,
+      desc = 'Git Status',
+    },
+    {
+      '<leader>gS',
+      function()
+        Snacks.picker.git_stash()
+      end,
+      desc = 'Git Stash',
+    },
+    {
+      '<leader>gd',
+      function()
+        Snacks.picker.git_diff()
+      end,
+      desc = 'Git Diff (Hunks)',
+    },
+    {
+      '<leader>gf',
+      function()
+        Snacks.picker.git_log_file()
+      end,
+      desc = 'Git Log File',
     },
   },
 }
