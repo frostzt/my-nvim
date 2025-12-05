a86e57cc600626cd63d684ffd47b6e5e424dc1f4
frostzt <aidenfrostbite@gmail.com>
2025-12-06 02:17:18 +0530

    feat(opencode): moved to sudo-tee for opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,36 +1,17 @@
 return {
-  'NickvanDyke/opencode.nvim',
+  'sudo-tee/opencode.nvim',
   config = function()
-    -- Required for `opts.events.reload`.
-    vim.o.autoread = true
-
-    -- Recommended/example keymaps.
-    vim.keymap.set({ 'n', 'x' }, '<C-a>', function()
-      require('opencode').ask('@this: ', { submit = true })
-    end, { desc = 'Ask opencode' })
-
-    vim.keymap.set({ 'n', 'x' }, '<C-x>', function()
-      require('opencode').select()
-    end, { desc = 'Execute opencode action…' })
-
-    vim.keymap.set({ 'n', 'x' }, 'ga', function()
-      require('opencode').prompt '@this'
-    end, { desc = 'Add to opencode' })
-
-    vim.keymap.set({ 'n', 't' }, '<C-.>', function()
-      require('opencode').toggle()
-    end, { desc = 'Toggle opencode' })
-
-    vim.keymap.set('n', '<S-C-u>', function()
-      require('opencode').command 'session.half.page.up'
-    end, { desc = 'opencode half page up' })
-
-    vim.keymap.set('n', '<S-C-d>', function()
-      require('opencode').command 'session.half.page.down'
-    end, { desc = 'opencode half page down' })
-
-    -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above — otherwise consider "<leader>o".
-    vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
-    vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement', noremap = true })
+    require('opencode').setup {}
   end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        anti_conceal = { enabled = false },
+        file_types = { 'markdown', 'opencode_output' },
+      },
+      ft = { 'markdown', 'Avante', 'copilot-chat', 'opencode_output' },
+    },
+  },
 }
