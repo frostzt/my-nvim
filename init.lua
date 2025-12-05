31704a4a235f45146472418631ad2213f819bf2a
frostzt <aidenfrostbite@gmail.com>
2025-12-05 13:04:28 +0530

    fix: treesitter and lsp config

--- init.lua
+++ init.lua
@@ -3,8 +3,8 @@ vim.g.maplocalleader = ' '
 
 require 'core.autocmds'
 require 'core.keymaps'
-require 'core.lazy'
 require 'core.options'
+require 'core.lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -13,10 +13,9 @@ return {
     },
     indent = { enable = true, disable = { 'ruby' } },
   },
-  -- There are additional nvim-treesitter modules that you can use to interact
-  -- with nvim-treesitter. You should go explore a few and see what interests you:
-  --
-  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {},
+  },
 }
