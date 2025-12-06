72991c107a8f99a8bcaced47c526ac9687ac42a6
frostzt <aidenfrostbite@gmail.com>
2025-12-06 19:55:02 +0530

    chore: removed animations

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -76,9 +76,7 @@ return {
     'saecki/crates.nvim',
     event = { 'BufRead Cargo.toml' },
     opts = {
-      completion = {
-        cmp = { enabled = false },
-      },
+      completion = {},
       lsp = {
         enabled = true,
         actions = true,
