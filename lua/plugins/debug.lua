0106a9cfa6be082fdc53a33c3a78b3dbe6d7c3b0
frostzt <aidenfrostbite@gmail.com>
2025-12-08 14:25:31 +0530

    chore: cleanup boilerplate

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,17 +2,15 @@
 return {
   'mfussenegger/nvim-dap',
   dependencies = {
-    -- Creates a beautiful debugger UI
+    -- UI
     'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
 
-    -- Installs the debug adapters for you
+    -- Installers
     'mason-org/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
+    -- Debuggers
     'leoluz/nvim-dap-go',
   },
   keys = {
@@ -73,18 +71,9 @@ return {
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
       },
     }
